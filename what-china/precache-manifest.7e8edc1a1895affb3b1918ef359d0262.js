3451b26a94c2df93c52caf87b0ddf12e1403650a
andy <historykly@gmail.com>
2020-10-14 09:59:34 +0800

    修改一下页面

---
+++ what-china/precache-manifest.7e8edc1a1895affb3b1918ef359d0262.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a23f80702a034cd577eed53cf7feeaed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3489d520cc199fc4cd3d",
+    "url": "./static/css/main.db9da793.chunk.css"
+  },
+  {
+    "revision": "e3bf0d4486dccd7d13e4",
+    "url": "./static/js/2.220ac357.chunk.js"
+  },
+  {
+    "revision": "f5c102b30733f3da34bf",
+    "url": "./static/js/3.09b66bd8.chunk.js"
+  },
+  {
+    "revision": "3489d520cc199fc4cd3d",
+    "url": "./static/js/main.a2b0fb71.chunk.js"
+  },
+  {
+    "revision": "3089f51597c951c0ffd1",
+    "url": "./static/js/runtime~main.dce1b889.js"
+  },
+  {
+    "revision": "ce158e82124ad597450b17664abe5cc9",
+    "url": "./static/media/11.ce158e82.png"
+  },
+  {
+    "revision": "17c499717fc1d92171ba7e33f18c5b78",
+    "url": "./static/media/12.17c49971.png"
+  },
+  {
+    "revision": "a6934414b1c804165e059367f3a03396",
+    "url": "./static/media/13.a6934414.png"
+  },
+  {
+    "revision": "1bbe3460b053c4f1d67d1f1afecdaa76",
+    "url": "./static/media/DIN-Medium.1bbe3460.otf"
+  },
+  {
+    "revision": "8648fa69186be85c794ccd1dee3303ca",
+    "url": "./static/media/SourceHanSerifCN-Bold.8648fa69.otf"
+  },
+  {
+    "revision": "bf30896171239d07c212d0d97f428ed2",
+    "url": "./static/media/WechatIMG364.bf308961.jpg"
+  },
+  {
+    "revision": "3400685b61d626be70098a2cb5e1fe16",
+    "url": "./static/media/difangzhengfu.3400685b.png"
+  },
+  {
+    "revision": "37b7309d5b1914a18d53e7114696c51b",
+    "url": "./static/media/guowu.37b7309d.png"
+  },
+  {
+    "revision": "22a6618ccb1925a52eb66e30824d7a94",
+    "url": "./static/media/titles.22a6618c.png"
+  },
+  {
+    "revision": "1559dc73256145137bdac9e625c6be71",
+    "url": "./static/media/waijiaojigou.1559dc73.png"
+  }
+]);
